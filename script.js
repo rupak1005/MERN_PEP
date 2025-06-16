9f24dcde342b789f80b32dd6b92ba3a6cd6bc33c
rupak1005 <rupak1005saini@gmail.com>
2025-06-16 14:57:37 +0530

    guessing game

--- script.js
+++ script.js
@@ -385,3 +385,45 @@
 
 // }
 // console.log(time(86400))
+
+
+function longestword(str) {
+    var words = str.split(" ");
+    var maxlength = 0;
+    var longest = "";
+
+    words.forEach(element => {
+        if (maxlength < element.length) {
+            maxlength = element.length;
+            longest = element;
+        }
+    });
+
+    return longest;
+}
+
+console.log(longestword("hello yogyuguo miss"))
+
+
+function guess() {
+    var secnumber = Math.floor(Math.random() * 100) + 1; // random number between 1 and 100
+    var number;
+    var chances = 7;
+    console.log("Secret number (for testing):", secnumber);
+
+    while (chances > 0) {
+        number = Number(prompt(`Guess the number (1-100):`));
+        chances--;
+
+        if (number === secnumber) {
+            return 'You win!';
+        } else if (number > secnumber) {
+            alert(`Secret number is smaller. Tries left: ${chances}`);
+        } else {
+            alert(`Secret number is greater. Tries left: ${chances}`);
+        }
+    }
+    return alert("You lose!");
+}
+
+console.log(guess());
