551cd61517b2e0736940eb23536487ff2f314966
rupak1005 <rupak1005saini@gmail.com>
2025-06-16 11:16:51 +0530

    js basics

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+"use strict"
 // Array operations
+
 // const arr=["ankit","rupak",1,2];
 
 // Using forEach to iterate over array elements
@@ -26,7 +28,24 @@ var obj = {
 }
 console.log('name = ' + obj.name); // Accessing object property
 
+for(var i in obj){
+    console.log(i,":",obj[i])
+}
 // Class definition and instantiation
+/**
+ * Represents a person with a name and age.
+ *
+ * @class yo
+ * @classdesc This class is used to create an object representing a person, storing their name and age as properties.
+ *
+ * @param {string} name - The name of the person.
+ * @param {number} age - The age of the person.
+ *
+ * @example
+ * const person = new yo('Alice', 30);
+ * console.log(person.name); // 'Alice'
+ * console.log(person.age);  // 30
+ */
 class yo {
     constructor(name, age) {
         this.name = name;
@@ -35,3 +54,8 @@ class yo {
 }
 const obj1 = new yo('ankit', 22);
 console.log('name = ' + obj1.name); // Accessing class instance property
+
+{
+    var a=1
+}
+console.log(a)
