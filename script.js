2423bdde8acf0425ec86ca2e7858df3056896553
rupak1005 <rupak1005saini@gmail.com>
2025-06-16 11:55:57 +0530

    func vs arrow

--- script.js
+++ script.js
@@ -59,3 +59,52 @@ console.log('name = ' + obj1.name); // Accessing class instance property
     var a=1
 }
 console.log(a)
+
+// Notes: Function Arguments and Parameters in JavaScript
+
+// - JavaScript functions are flexible with arguments:
+//   - You can call a function with more, fewer, or exactly the number of parameters it declares.
+//   - No error is thrown if the number of arguments doesn't match the number of parameters.
+
+// - Parameters vs. Arguments:
+//   - Parameters: Named variables in the function definition.
+//   - Arguments: Actual values passed when calling the function.
+
+// - The 'arguments' object:
+//   - Every non-arrow function has access to an array-like 'arguments' object.
+//   - It contains all arguments passed to the function, even if no parameters are declared.
+
+// Example:
+/**
+ * Logs the arguments object to the console.
+ * 
+ * @function
+ * @returns {void}
+ * @note This function does not accept any named parameters, 
+ * but can access all arguments passed to it via the `arguments` object.
+ */
+function noParams() {
+    console.log(arguments);
+}
+noParams(1, 2, 3); // Output: [Arguments] { '0': 1, '1': 2, '2': 3 }
+
+// - Extra arguments are available in 'arguments'.
+// - Missing parameters are set to undefined.
+// - JavaScript does not enforce arity (number of parameters).
+
+// Notes: Function Declarations vs. Arrow Functions
+
+// 1. Function Declaration
+// - Traditional way to define a function.
+// - Can be hoisted (used before declaration).
+function multiply1(a, b) {
+    return a + b; // Note: This actually adds, not multiplies.
+}
+
+// 2. Arrow Function
+// - Introduced in ES6.
+// - Shorter syntax, does not have its own 'this' or 'arguments'.
+const multiply = (a, b) => a * b;
+
+// Example usage:
+console.log(multiply(2, 4)); // Output: 8
