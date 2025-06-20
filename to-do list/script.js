4703d3b4c61b7fd471142f3aeec060641e6ce418
rupak1005 <rupak1005saini@gmail.com>
2025-06-20 11:34:44 +0530

    modified

--- to-do list/script.js
+++ to-do list/script.js
@@ -6,7 +6,6 @@ add.addEventListener("click",()=>{
     if(textarea.value === "") return;
     addlist.innerHTML+=`<li>${textarea.value} <button class="remove"> remove</button></li>
      `;
-    console.log("hi");
     textarea.value="";
 })
 addlist.addEventListener("click",(elem)=>{
