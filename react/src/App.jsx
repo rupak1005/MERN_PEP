3c65aa804ea279bb742c942edb60c83174e6eff2
rupak1005 <rupak1005saini@gmail.com>
2025-06-25 09:52:29 +0530

    feat: initialize React project with Vite, Tailwind CSS, and ESLint setup
    - Added package.json with dependencies for React, ReactDOM, Vite, and Tailwind CSS.
    - Configured PostCSS with Tailwind CSS and Autoprefixer.
    - Created main application component (App.jsx) and basic structure.
    - Included SVG assets for Vite and React logos.
    - Set up global CSS with Tailwind directives.
    - Configured Vite for React with necessary plugins.
    - Added Tailwind CSS configuration for content paths.
    - Implemented main entry point (main.jsx) for rendering the application.

---
+++ react/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className=''>
+      App Component 
+    </div>
+  );
+}
+
+export default App;
---
+++ react/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // adjust as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
