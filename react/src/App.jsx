f2881b66e03e3b614023fc9d16451544f1ad484b
rupak1005 <rupak1005saini@gmail.com>
2025-06-25 14:26:42 +0530

    feat: integrate new components into App.jsx
    - Imported Form, Counter, and UseState components into the main App component.
    - Updated the JSX structure to include UseState while commenting out Form and Counter for future use.

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import Form from './components/Form';
+import Counter from './components/Counter';
+import UseState from './components/UseState';
 function App() {
   return (
-    <div className=''>
-      App Component 
+    <div>
+      {/* <Form /> */}
+      {/* <Counter /> */}
+      <UseState />
     </div>
   );
 }
