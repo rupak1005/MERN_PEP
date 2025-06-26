257911c7b5dac32477b52d39bb0d9a9c3303f3a5
rupak1005 <rupak1005saini@gmail.com>
2025-06-26 14:33:08 +0530

    fixx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,16 +4,16 @@ import Counter from './components/Counter';
 import UseState from './components/UseState';
 import Todolist from './components/Todolist';
 import Products from './components/Products';
-import { Button } from './components/ui/button';
+// import { Button } from './components/ui/button';
 import './index.css'
 function App() {
   return (
     <div>
-      {/* <Form /> */}
-      {/* <Counter /> */}
-      {/* <UseState /> */}
-      {/* <Todolist /> */}
-      {/* <Products /> */}
+      {/* <Form />
+      <Counter />
+      <UseState />
+      <Todolist />
+      <Products /> */}
       {/* <Button className="bg-red-500 hover:bg-red-300 text-white font-bold py-2 px-4 rounded-full shadow-lg transform hover:scale-105 transition-all duration-200">
         Click me
       </Button> */}
--- react/src/components/Products.jsx
+++ react/src/components/Products.jsx
@@ -13,8 +13,8 @@ function Products() {
 
 
     return (
-        <div className="container mx-auto p-6">
-            <h1 className="text-3xl font-bold text-center mb-8 text-gray-800">Products</h1>
+        <div className="container mx-auto p-6 bg-zinc-100 min-w-full">
+            <h1 className="text-3xl font-bold text-center mb-8 text-gray-800 uppercase">Products</h1>
             <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {products.map(product => (
                     <div key={product.id} className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300 ">
