22de0b2fa1ec52b74a1df4378035ca6391e08a30
rupak1005 <rupak1005saini@gmail.com>
2025-06-26 12:00:02 +0530

    chore: update dependencies and enhance Tailwind CSS configuration
    - Added new dependencies: @radix-ui/react-slot, class-variance-authority, clsx, lucide-react, shadcn-ui, tailwind-merge, and tailwindcss-animate.
    - Updated package-lock.json and package.json to reflect the new dependencies.
    - Enhanced Tailwind CSS configuration with custom theme settings for colors and border radius.
    - Configured Vite to resolve paths for easier imports.
    - Updated global CSS with new design tokens for dark mode support.

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,6 +4,8 @@ import Counter from './components/Counter';
 import UseState from './components/UseState';
 import Todolist from './components/Todolist';
 import Products from './components/Products';
+import { Button } from './components/ui/button';
+import './index.css'
 function App() {
   return (
     <div>
@@ -12,6 +14,9 @@ function App() {
       {/* <UseState /> */}
       {/* <Todolist /> */}
       {/* <Products /> */}
+      {/* <Button className="bg-red-500 hover:bg-red-300 text-white font-bold py-2 px-4 rounded-full shadow-lg transform hover:scale-105 transition-all duration-200">
+        Click me
+      </Button> */}
       
     </div>
   );
