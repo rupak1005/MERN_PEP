1cce3339aad279869730a02593255b3b54b3a687
rupak1005 <rupak1005saini@gmail.com>
2025-07-01 20:48:47 +0530

    commit

--- REACT2/src/App.jsx
+++ REACT2/src/App.jsx
@@ -10,15 +10,17 @@ function App() {
     <div className='p-1 w-1/2 m-auto'>
        <nav className='p-5 m-10 bg-zinc-300 rounded justify-center flex  gap-10 '>
         <Link to="/">Home</Link>
-        <Link to="/User">User</Link>
-        <Link to="/About">About</Link>
+        <Link to="/user">User</Link>
+        <Link to="/about">About</Link>
       </nav>
-      <Routes className="p-5 m-10 w-1/2 rounded justify-center flex  gap-10 ">
-        <Route path="/" element={<Home />} />
-        <Route path="/User" element={<User />} />
-        <Route path="/user/:id" element={<UserDetail />} />
-        <Route path="/About" element={<About />} />
-      </Routes>
+      <div className="p-5 m-10 w-1/2 rounded justify-center flex gap-10">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/user/:id" element={<UserDetail />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </div>
   );
 }
